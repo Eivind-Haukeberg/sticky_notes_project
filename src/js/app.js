19f20655d3ceab97878f2e04a6399cdb22746d3c
Eivind Haukeberg <e.haukeberg@gmail.com>
2025-01-15 13:15:15 +0100

    Implement add notes feature

---
+++ src/js/app.js
@@ -0,0 +1,27 @@
+// Selecting elements
+
+import addNotes from "./addNotes.js";
+
+const form = document.querySelector(".form");
+const subjectInput = document.querySelector(".form__subject-input");
+const dateInput = document.querySelector(".form__date-input");
+const noteText = document.querySelector(".form__note-input");
+
+
+
+
+
+
+
+
+
+
+
+
+// Add submit event to the form
+
+form.addEventListener("submit", (e)=> {
+    e.preventDefault();
+    addNotes(subjectInput,dateInput,noteText)
+});
+
